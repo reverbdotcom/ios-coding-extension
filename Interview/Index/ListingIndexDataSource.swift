fcf90c55fe2dc2698ce82c48cafd34acf00437c0
zachcmielreverb <zachary.cmiel@reverb.com>
2026-07-23 13:35:22 -0500

    Fix indentation

--- Interview/Index/ListingIndexDataSource.swift
+++ Interview/Index/ListingIndexDataSource.swift
@@ -3,32 +3,31 @@ import UIKit
 typealias JSONObject = [AnyHashable: Any]
 
 final class ListingIndexDataSource: NSObject {
-  var products = [JSONObject]()
+    var products = [JSONObject]()
 }
 
 extension ListingIndexDataSource: UITableViewDataSource {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return products.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell : UITableViewCell
-    if let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: "ProductCell") {
-      cell = dequeuedCell
-      
-    } else {
-      cell = UITableViewCell(style: .default, reuseIdentifier: "ProductCell")
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    
-    let relatedItem = products[indexPath.item]
-    
-    cell.textLabel?.text = relatedItem["title"] as? String
-    cell.textLabel?.numberOfLines = 2
-    
-    return cell
-  }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell
+        if let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: "ProductCell") {
+            cell = dequeuedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "ProductCell")
+        }
+
+        let relatedItem = products[indexPath.item]
+
+        cell.textLabel?.text = relatedItem["title"] as? String
+        cell.textLabel?.numberOfLines = 2
+
+        return cell
+    }
 }
--- Interview/Index/ListingIndexViewController.swift
+++ Interview/Index/ListingIndexViewController.swift
@@ -2,41 +2,41 @@ import UIKit
 
 final class ListingIndexViewController: UIViewController {
 
-  weak var tableView: UITableView!
-  var dataSource = ListingIndexDataSource()
+    weak var tableView: UITableView!
+    var dataSource = ListingIndexDataSource()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    navigationItem.title = "Gear"
-    tableView = makeTableView()
-    fetchData()
-  }
+        navigationItem.title = "Gear"
+        tableView = makeTableView()
+        fetchData()
+    }
 }
 
 extension ListingIndexViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-  }
+    }
 }
 
 private extension ListingIndexViewController {
-  func makeTableView() -> UITableView {
-    let tableView = UITableView(frame: view.frame)
-    view.addSubview(tableView)
-    tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    tableView.dataSource = dataSource
-    tableView.delegate = self
-    tableView.rowHeight = 60
-    return tableView
-  }
+    func makeTableView() -> UITableView {
+        let tableView = UITableView(frame: view.frame)
+        view.addSubview(tableView)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.dataSource = dataSource
+        tableView.delegate = self
+        tableView.rowHeight = 60
+        return tableView
+    }
 
-  func fetchData() {
-    let path = Bundle.main.url(forResource: "reverb-gear-index", withExtension: "json")!
-    let data = try! Data(contentsOf: path)
-    let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [JSONObject]
-    dataSource.products = json
-    
-    tableView.reloadData()
-  }
+    func fetchData() {
+        let path = Bundle.main.url(forResource: "reverb-gear-index", withExtension: "json")!
+        let data = try! Data(contentsOf: path)
+        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [JSONObject]
+        dataSource.products = json
+
+        tableView.reloadData()
+    }
 }
