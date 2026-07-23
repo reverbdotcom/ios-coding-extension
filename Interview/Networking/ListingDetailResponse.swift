9f175c4eced983d22bb57c4a8cce8fa2232f6bca
zachcmielreverb <zachary.cmiel@reverb.com>
2026-07-23 15:09:02 -0500

    Flatten

--- Interview/Networking/ListingDetailResponse.swift
+++ Interview/Networking/ListingDetailResponse.swift
@@ -1,32 +1,35 @@
 import Foundation
 
 /// Represents the JSON response of a ``ListingDetailRequest``.
-
 struct ListingDetailResponse: Decodable {
     let data: ResponseData
+}
 
+extension ListingDetailResponse {
     struct ResponseData: Decodable {
         let listing: ListingDetail
+    }
+}
 
-        struct ListingDetail: Decodable {
-          let id: String
-          let title: String
-          let description: String
-          let price: Price
-          let shippingPrice: ShippingPrice
-          let images: [Image]
+extension ListingDetailResponse {
+    struct ListingDetail: Decodable {
+        let id: String
+        let title: String
+        let description: String
+        let price: Price
+        let shippingPrice: ShippingPrice
+        let images: [Image]
 
-          struct Price: Decodable {
+        struct Price: Decodable {
             let display: String
-          }
+        }
 
-          struct ShippingPrice: Decodable {
+        struct ShippingPrice: Decodable {
             let display: String
-          }
+        }
 
-          struct Image: Decodable {
+        struct Image: Decodable {
             let source: URL?
-          }
         }
     }
 }
--- Interview/Networking/ListingsSearchResponse.swift
+++ Interview/Networking/ListingsSearchResponse.swift
@@ -3,17 +3,21 @@ import Foundation
 /// Represents the JSON response of a ``ListingsSearchRequest``.
 struct ListingsSearchResponse: Decodable {
     let data: ResponseData
+}
 
+extension ListingsSearchResponse {
     struct ResponseData: Decodable {
         let listingsSearch: ListingsSearch
+    }
+}
 
-        struct ListingsSearch: Decodable {
-            let listings: [Listing]
+extension ListingsSearchResponse {
+    struct ListingsSearch: Decodable {
+        let listings: [Listing]
 
-            struct Listing: Decodable {
-                let id: String
-                let title: String
-            }
+        struct Listing: Decodable {
+            let id: String
+            let title: String
         }
     }
 }
