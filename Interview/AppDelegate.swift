ae30ffc5936cd13e040c32042607c11075f2d2e1
JohnBueno <john.mccartney@reverb.com>
2026-08-03 10:43:44 -0400

    remove storyboard

--- Interview/AppDelegate.swift
+++ Interview/AppDelegate.swift
@@ -5,6 +5,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: ListingIndexViewController())
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 }
